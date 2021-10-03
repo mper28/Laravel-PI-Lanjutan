be8da088e8217d487325feecdac85ea6980975b2
John DoeRamadhan <ramadhan.feriansyah28@gmail.com>
2021-10-04 01:08:53 +0700

    ini adalah tugas minggu pertama

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\cobacontroller;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/coba', function () {
+    return view(' coba');
+});
+
+Route::get('/coba/{no}', function ($no) {
+    return 'coba ke-' . $no;
+});
+
+Route::get('/test', [cobacontroller::class, 'index']);
+
+Route::get('/test/{ke}', [cobacontroller::class, 'urutan']);
